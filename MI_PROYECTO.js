da49803e4504a61a91f3aa8123d8501eed00f977
Luis Lopez <luisfl716@gmail.com>
2023-02-23 22:05:55 -0300

    Escritura de Repositorio

---
+++ MI_PROYECTO.js
@@ -0,0 +1,46 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//el menu lateral se oculta luego de elegir una de las opciones
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+/* ***************************** */
+
+
+/* ***************************** */
+//Progreso de la barra de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("mecanica");
+        habilidades[1].classList.add("mantenimiento");
+        habilidades[2].classList.add("electricidad");
+        habilidades[3].classList.add("diseño");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
+    }
+}
+
+//Aplica la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
